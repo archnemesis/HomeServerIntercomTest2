4ec040f2d2e2b83154c72d629f4bc97c0983b565
Robin Gingras <robin@robingingras.com>
2018-03-24 22:16:18 -0700

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,98 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+#define FRAME_SIZE 320
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->DisconnectButton->setEnabled(false);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_connectButton_clicked()
+{
+    QHostAddress addr(ui->hostAddress->text());
+    int port = 2050;
+
+    m_udpSocket = new QUdpSocket(this);
+    m_udpSocket->bind(port);
+    connect(m_udpSocket, &QUdpSocket::readyRead, this, &MainWindow::on_udpSocket_readyRead);
+
+    ui->hostAddress->setEnabled(false);
+    ui->hostPort->setEnabled(false);
+    ui->connectButton->setEnabled(false);
+    ui->DisconnectButton->setEnabled(true);
+
+    QAudioFormat format;
+    format.setSampleRate(16000);
+    format.setChannelCount(1);
+    format.setSampleSize(16);
+    format.setCodec("audio/pcm");
+    format.setByteOrder(QAudioFormat::LittleEndian);
+    format.setSampleType(QAudioFormat::SignedInt);
+
+    QAudioDeviceInfo info = QAudioDeviceInfo::defaultInputDevice();
+    if (!info.isFormatSupported(format)) {
+        qWarning() << "Default format unsupported!";
+        return;
+    }
+
+    m_audioInput = new QAudioInput(format, this);
+    m_audioInput->setNotifyInterval(10);
+    m_audioInput->setBufferSize(FRAME_SIZE * 2);
+    connect(m_audioInput, &QAudioInput::stateChanged, this, &MainWindow::on_audioInput_stateChanged);
+    connect(m_audioInput, &QAudioInput::notify, this, &MainWindow::on_audioInput_notify);
+
+    m_audioInputDevice = m_audioInput->start();
+}
+
+void MainWindow::on_audioInput_stateChanged(QAudio::State state)
+{
+    switch (state) {
+    case QAudio::ActiveState:
+        qDebug("QAudio now in active state!");
+        break;
+    case QAudio::SuspendedState:
+        qDebug("QAudio now suspended");
+        break;
+    case QAudio::StoppedState:
+        qDebug("QAudio now stopped");
+        break;
+    case QAudio::IdleState:
+        qDebug("QAudio now idle");
+        break;
+    default:
+        qDebug("Other state...");
+        break;
+    }
+}
+
+void MainWindow::on_audioInput_notify()
+{
+    QByteArray buf = m_audioInputDevice->read(FRAME_SIZE);
+    m_udpSocket->writeDatagram(buf, QHostAddress("10.1.1.141"), 2050);
+    ui->audioBufferProgress->setValue(m_audioInput->bytesReady());
+}
+
+void MainWindow::on_udpSocket_readyRead()
+{
+
+}
+
+void MainWindow::on_DisconnectButton_clicked()
+{
+    m_udpSocket->close();
+
+    ui->hostAddress->setEnabled(true);
+    ui->hostPort->setEnabled(true);
+    ui->connectButton->setEnabled(true);
+    ui->DisconnectButton->setEnabled(false);
+}
---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QAudioInput>
+#include <QAudioFormat>
+#include <QAudioDeviceInfo>
+#include <QUdpSocket>
+#include <QHostAddress>
+#include <QNetworkDatagram>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_connectButton_clicked();
+    void on_audioInput_stateChanged(QAudio::State state);
+    void on_audioInput_notify();
+    void on_udpSocket_readyRead();
+    void on_DisconnectButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QAudioInput *m_audioInput;
+    QUdpSocket *m_udpSocket;
+    QIODevice *m_audioInputDevice;
+};
+
+#endif // MAINWINDOW_H
